f37eed347800b52d0ccf0dc35bfa2779a80a29cc
Kurairu <98249948+kurairu-codes@users.noreply.github.com>
2023-10-01 14:54:08 +0900

    css & template update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from.models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -8,4 +9,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     # posts = Post.objects.all()
     print(posts)
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/templates/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
